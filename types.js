08d1d1fdb9f9c5abf9dd819b93db1b9dde977e35
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-04-14 15:52:19 -0700

    data types almost complete

--- types.js
+++ types.js
@@ -1,1 +1,25 @@
-console.log('Hello from ts');
+"use strict";
+exports.__esModule = true;
+var myString;
+var myNum;
+var myBool;
+var myVar;
+myString = 'Hello World';
+// can be positve, negative, expressions, decimals, hexidecimal
+myNum = 1;
+myBool = true;
+myVar = "This can be anything";
+var strArray;
+strArray = ['Hello', 'World'];
+// can declare arrays with angle brackets
+var numArray;
+numArray = [1, 2, 3, 4, 5];
+var boolArray;
+boolArray = [true, false, true];
+//tuples
+var strNumTuple;
+//data has to match whatever pattern is given at declaration, but the IDE will show an error
+strNumTuple = ['Association List', 4.0];
+console.log(myString + ' ' + myNum + ' ' + myBool
+    + ' ' + myVar + ' ' + strArray
+    + ' ' + numArray + ' ' + boolArray + ' ' + strNumTuple);
--- types.ts
+++ types.ts
@@ -1,1 +1,29 @@
-console.log('Hello from ts')
+// tsc types.ts -w
+export {};
+let myString: string;
+let myNum: number;
+let myBool: boolean;
+let myVar: any;
+
+myString = 'Hello World';
+// can be positve, negative, expressions, decimals, hexidecimal
+myNum = 1;
+myBool = true;
+myVar = "This can be anything";
+
+let strArray: string[];
+strArray = ['Hello', 'World']
+// can declare arrays with angle brackets
+let numArray: Array<number>;
+numArray = [1,2,3,4,5];
+let boolArray: boolean[];
+boolArray = [true,false,true];
+//tuples
+let strNumTuple: [string, number];
+//data has to match whatever pattern is given at declaration, but the IDE will show an error
+strNumTuple = ['Association List', 4.0];
+
+console.log(myString + ' ' + myNum + ' ' + myBool 
+    + ' ' + myVar + ' ' + strArray 
+    + ' ' + numArray + ' ' + boolArray + ' ' + strNumTuple
+)
